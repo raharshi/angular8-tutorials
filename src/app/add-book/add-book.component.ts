6de5b0ab86e509649ba77d00616364f50693c0c8
rishi puli <riskybugg@gmail.com>
2020-03-23 20:42:44 +0530

    new comp added

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Book } from '../services/book';
+import { BookService } from '../services/book.service';
 
 @Component({
-  selector: 'app-add-book',
-  templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.scss']
+    selector: 'add-book-app',
+    templateUrl: './add-book.component.html',
+    styleUrls: ['./add-book.component.scss']
 })
-export class AddBookComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class AddBookComponent implements OnInit { 
+    books: Book[];
+    book: Book = new Book();
+    constructor(private router: Router,
+	        private bookService: BookService) { }
+    getBooks(): void {
+        this.bookService.getBooks().then(books => this.books = books);
+    }
+    ngOnInit(): void {
+        this.getBooks();
+    }
+    addBook(): void {
+	this.bookService.addBook(this.book);
+        this.router.navigate(['/home']);
+    }	
+} 
--- src/app/manage-book/manage-book.component.ts
+++ src/app/manage-book/manage-book.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Book } from '../services/book';
+import { BookService } from '../services/book.service';
 
 @Component({
-  selector: 'app-manage-book',
-  templateUrl: './manage-book.component.html',
-  styleUrls: ['./manage-book.component.scss']
+    selector: 'manage-book-app',
+    templateUrl: './manage-book.component.html',
+    styleUrls: ['./manage-book.component.scss']
 })
-export class ManageBookComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ManageBookComponent implements OnInit { 
+    books: Book[];
+    book: Book = new Book();
+    constructor(private router: Router,
+	        private bookService: BookService) { }
+    getBooks(): void {
+        this.bookService.getBooks().then(books => this.books = books);
+    }
+    ngOnInit(): void {
+        this.getBooks();
+    }
+    updateBook(id:number): void {
+	this.router.navigate(['/update-book', id]);
+    }
+    deleteBook(id:number): void {
+	this.bookService.deleteBook(id);
+    }
+} 
