0e4ff26ff9128a7e224bd4af718edcce72c58074
rishi puli <riskybugg@gmail.com>
2020-03-23 19:39:21 +0530

    new comp added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Book } from '../services/book';
+import { BookService } from '../services/book.service';
+
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'home-app',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class HomeComponent implements OnInit { 
+    books: Book[];
+    constructor(private bookService: BookService) { }
+    getBooks(): void {
+        this.bookService.getBooks().then(books => this.books = books);
+    }
+    ngOnInit(): void {
+        this.getBooks();
+    }				
+} 
